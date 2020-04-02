587543b5e908a0d11a87b0aaf0608652802c53f7
XiomaraJimenez <xiomararosado.0491@hotmail.com>
2020-04-02 22:13:20 +0000

    ‘commit’

---
+++ Lab2.js
@@ -0,0 +1,69 @@
+/*ar myALphabet = ['A','B','C','D','E','F','G'];
+myALphabet.length;
+console.log(myALphabet.length);
+
+var Planets = ['Venus', 'Jupiter', 'Earth', 'Mars','Mercury'];
+var first = Planets[0];
+console.log(first);
+var Planets = ['Venus', 'Jupiter', 'Earth', 'Mars','Mercury'];
+var first = Planets[1];
+console.log(first);
+var Planets = ['Venus', 'Jupiter', 'Earth', 'Mars','Mercury'];
+var first = Planets[2];
+console.log(first);
+var Planets = ['Venus', 'Jupiter', 'Earth', 'Mars','Mercury'];
+var first = Planets[3];
+console.log(first);
+var Planets = ['Venus', 'Jupiter', 'Earth', 'Mars','Mercury'];
+var first = Planets[4];
+console.log(first); 
+*/
+var x = 5, y = 10, z = 15;
+Result= x+y;
+Result1= y-x;
+Result2 = x*y;
+Result3 = y / x;
+Result4 = x % 2;
+Result5 = x++;
+Result6 = x--;
+console.log(Result, Result1, Result2, Result3, Result4, Result5, Result6);
+
+var a = 5, b ='Hello', c = 'World', d = 10;
+console.log(a  +' '+ b);
+
+var a = 5, b ='Hello', c = 'World', d = 10;
+console.log(b  +' '+ c);
+
+var a = 5, b ='Hello', c = 'World', d = 10;
+console.log(a  +' '+ d);
+
+
+var a = 5, b = 10, c = "5";
+var x = a;
+console.log(a == c);
+console.log(a === c);
+
+console.log(a == x);
+
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a >= c);
+console.log(a <=c);
+
+
+var hello = function()
+{
+    return 'Hello World!';
+};
+var x = hello ();
+
+console.log(x);
+
+var hello = function(){
+    return 'Hi!';
+};
+var x = hello();
+console.log(x);
